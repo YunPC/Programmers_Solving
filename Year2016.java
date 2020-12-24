e6159db378dc3197715a644aaa5fbbf91e213bde
YunPC <46584354+YunPC@users.noreply.github.com>
2020-12-24 12:12:39 +0900

    create GyunSuit, Year2016

---
+++ Year2016.java
@@ -0,0 +1,23 @@
+public class Year2016 {
+
+    public static void main(String[] args)
+    {
+        int a = 1;
+        int b = 3;
+        String answer = "";
+        int[] months = {0,31, 29, 31, 30, 31, 30, 31, 31, 30, 31, 30};
+        String[] days = {"MON", "TUE", "WED", "THU", "FRI", "SAT", "SUN"};
+        int day = 4;
+        int month = 1;
+        while(month != a)
+        {
+            day += months[month];
+            month++;
+        }
+        day += b-1;
+        day %= 7;
+        answer = days[day];
+        System.out.println(answer);
+    }
+    
+}
