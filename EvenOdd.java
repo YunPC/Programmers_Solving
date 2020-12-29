d8af02b01d47042ad2420c6e313c57ea8e0ca911
YunPC <46584354+YunPC@users.noreply.github.com>
2020-12-29 16:28:34 +0900

    짝수와 홀수 풀이

---
+++ EvenOdd.java
@@ -0,0 +1,11 @@
+public class EvenOdd {
+
+    //프로그래머스 문제 풀이 level1 짝수와 홀수
+    public static void main(String[] args)
+    {
+        int num = 3;
+        String answer = num % 2 == 0 ? "Even" : "Odd";
+        System.out.print(answer);
+    }
+    
+}
