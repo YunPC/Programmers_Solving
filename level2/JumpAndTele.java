050176dc47aad4b0e819725e4e7e10844d48d9b7
YunPC <46584354+YunPC@users.noreply.github.com>
2021-01-22 13:21:31 +0900

    점프와 순간이동 풀이

---
+++ level2/JumpAndTele.java
@@ -0,0 +1,13 @@
+package level2;
+
+public class JumpAndTele {
+
+    //프로그래머스 문제풀이 Level2 점프와 순간 이동
+
+    public static void main(String[] args) {
+        int n = 5000;
+        System.out.println(Integer.bitCount(n));
+    }
+    
+}
+
