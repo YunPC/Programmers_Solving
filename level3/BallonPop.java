796a12c745affe5ed815063f238979aee57c6c81
YunPC <apsn2000@nate.com>
2021-02-22 17:34:19 +0900

    풍선 터트리기 풀이

--- level3/BallonPop.java
+++ level3/BallonPop.java
@@ -5,43 +5,27 @@ public class BallonPop {
     //프로그래머스 문제풀이 level3 풍선 터트리기
 
     public static void main(String[] args) {
-        int[] a = {9,-1,-5};
-        int[] desc = new int[a.length];
-        int[] asc = new int[a.length];
+        int[] a = {9,1,5};
+        int[] left = new int[a.length];
+        int[] right = new int[a.length];
         
-        ArrayList<Integer> list = new ArrayList<>();
-        list.add(a[0]);
-        desc[0] = 1;
-        //내림차순 먼저
-        for(int i = 1; i < a.length; i++)
-        {
-            if(list.get(list.size()-1) > a[i])
-                list.add(a[i]);
-            else
-            {
-                boolean flag = false;
-                for(int j = list.size()-1; j >= 0; j--)
-                {
-                    if(list.get(j) > a[i])
-                    {
-                        list.remove(j--);
-                        list.add(j, a[i]);
-                        flag = true;
-                        break;
-                    }
-                }
-                if(!flag)
-                {
-                    list.remove(0);
-                    list.add(0, a[i]);
-                }
-                    
-            
-            }
-            desc[i] = list.size();
+        left[0] = a[0];
+        right[a.length-1] = a[a.length-1];
 
-        }
+        //left중에 최소값을 찾자
+        for(int i = 1; i < a.length; i++)
+            left[i] = left[i-1] > a[i] ? a[i] : left[i-1];
+
+        //right중에 최소값을 찾자
+        for(int i = a.length-2; i >= 0; i--)
+            right[i] = right[i+1] > a[i] ? a[i] : right[i+1];
+        
         int answer = 0;
+        //only one max!
+        for(int i = 0; i < a.length; i++)
+            answer = (left[i] < a[i]) && (right[i] < a[i]) ? answer : answer+1;
+        
+        System.out.println(answer);
     }
     
 }
